ad10a51d86311c2c766466b1dccd7d4be97ff1ac
JorgeMirandaM <jorge100099hotmail.com>
2023-11-02 14:37:34 -0600

    serve static content using express implemented

---
+++ app.js
@@ -0,0 +1,22 @@
+const express= require('express');
+const app= express();
+const port= 8080;
+
+//Servir contenido estático
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+    res.send('Hello world');
+});
+
+app.get('/hola-mundo',(req,res)=>{
+    res.send('Hola mundo en su respectiva ruta');
+});
+
+app.get('*',(req,res)=>{
+    res.sendFile(__dirname+'/public/404.html');
+});
+
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+})
